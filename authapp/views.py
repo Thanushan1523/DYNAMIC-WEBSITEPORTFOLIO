e6842571ce4069e2f620c1f10fdeb684bb966764
Thanushan1523 <thanushan1523@gmail.com>
2023-10-13 20:14:02 -0400

    developing login  and signup page with function in views

--- authapp/views.py
+++ authapp/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
 # Create your views here.
 def signup(request):
+    if request.method=="POST":
+        get_email=request.POST.get("email")
+        get_password =request.POST.get("pass1")
+        get_confirm_password =request.POST.get("pass2")
+        if get_password != get_confirm_password:
+            messages.info(request,'password is not matching')
+            return redirect('/auth/signup/')
+        try:
+            if User.objects.get(username=get_email):
+                messages.warning(request,"email is taken")
+                return redirect('/auth/signup/')
+        except Exception as identifier :
+            pass
+        myuser=User.objects.create_user(get_email,get_password)
+        myuser.save()
+        messages.success(request,'USER IS CREATED PLEASE LOGIN')
+        return redirect('/auth/login/')
+
     return render(request ,'signup.html')
 
 def handlelogin(request):
