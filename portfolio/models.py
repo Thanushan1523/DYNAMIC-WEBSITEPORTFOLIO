ff3e5566ada99e4eabd130eca89b19e5c706e532
Thanushan1523 <thanushan1523@gmail.com>
2023-10-17 09:19:58 -0400

    internship1

--- portfolio/models.py
+++ portfolio/models.py
@@ -20,3 +20,18 @@ class Blogs(models.Model):
 
     def __str__(self) :
         return self.tittle
+
+
+class Internship(models.Model):
+    fullname=models.CharField(max_length=60)
+    usn=models.CharField(max_length=60)
+    email=models.CharField(max_length=60)
+    collage_name=models.CharField(max_length=60)
+    offer_status=models.CharField(max_length=60)
+    start_date=models.CharField(max_length=60)
+    end_date=models.CharField(max_length=60)
+    proj_report=models.CharField(max_length=60)
+    timeStamp=models.DateTimeField(auto_now_add=True,blank=True)
+    
+    def __str__(self) :
+        return self.usn
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render ,redirect
 from django.contrib import messages
-from portfolio.models import Contact,Blogs
+from portfolio.models import Contact,Blogs,Internship
 # Create your views here.
 def home(request):
     return render(request ,'home.html')
@@ -16,10 +16,35 @@ def about(request):
     return render(request ,'about.html')
 
 def internshipdetails(request):
+    if not request.user.is_authenticated:
+        messages.warning(request, "please login to access this page")
+        return redirect('/auth/login/')
+
+    if request.method=="POST": 
+        fname=request.POST.get("name")
+        femail=request.POST.get("email")
+        fusn=request.POST.get("usn")
+        fcollege=request.POST.get("cname")
+        foffer=request.POST.get("offer")
+        fstartdate=request.POST.get("startdate")
+        fenddate=request.POST.get("enddate")
+        fproject=request.POST.get("project")
+
+
+        fname=fname.upper()
+        fusn=fusn.upper()
+        fcollege=fcollege.upper()
+        fproject=fproject.upper()
+        foffer=foffer.upper()
+
+        query=Internship(fullname=fname,usn=fusn, collage_name=fcollege,offer_status=foffer,start_date=fstartdate,end_date=fenddate,proj_report=fproject ,email=femail)
+        query.save()
+        messages.success(request, 'form is submitted')
+        return redirect ('/internship')
     return render(request ,'internshipdetails.html')
 
 def contact(request):
-    if request.method=="POST":
+    if request.method=="POST": 
         fname=request.POST.get("name")
         femail=request.POST.get("email")
         fphoneno=request.POST.get("num")
@@ -29,4 +54,4 @@ def contact(request):
         
         messages.success(request, 'thank you we will get back you soon')
         return redirect ('/contact')
-    return render(request ,'contact.html')
+    return render(request ,'contact.html') 
