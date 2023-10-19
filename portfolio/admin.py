4acf112d312b23493f727dc3af6f3998ade2fe17
Thanushan1523 <thanushan1523@gmail.com>
2023-10-19 08:05:20 -0400

    internship1 conected with admin listed in one coloum1

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -7,6 +7,6 @@ admin.site.register(Blogs)
 
 class InternshipAdmin(admin.ModelAdmin):
     list_display =('fullname','usn','email','collage_name','offer_status','start_date','end_date','proj_report','timeStamp')
-    
+    search_fields=('fullname','usn','email')
 
 admin.site.register(Internship,InternshipAdmin)
